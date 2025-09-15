34ab1eb5833fed78cf0bac0e6c7d051e06a3839e
1A12le5 <Abenizx909@gmail.com>
2025-09-16 01:46:11 +0800

    Remove files/folders moved to new branch

--- public/legacy/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "root", "123456789", "attendance_ip");
-if ($mysqli->connect_errno) {
-    http_response_code(500);
-    die(json_encode(["error" => "Failed to connect to MySQL: " . $mysqli->connect_error]));
-}
-$mysqli->set_charset("utf8mb4");
-?>
